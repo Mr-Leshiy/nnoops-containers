43b2e70ce10588a41d080023f51ded46e345ca83
Mr-Leshiy <leshiy12345678@gmail.com>
2021-03-07 16:32:44 +0200

    add map

--- src/nnoops/containers/map.hpp
+++ src/nnoops/containers/map.hpp
@@ -1,10 +1,19 @@
 #ifndef NNOOPS_CONTAINERS_MAP_HPP_
 #define NNOOPS_CONTAINERS_MAP_HPP_
 
+#include <type_traits>
+
+#include "nnoops/binary_trees/base_tree.hpp"
 namespace nnoops {
 
+template <typename tree_t>
+struct Map {
+  using node_t = typename tree_t::node_t;
 
-    
-}
+  using tmp_t = typename std::enable_if<
+      std::is_base_of<BaseTree<node_t>, tree_t>::value>::type;
+};
+
+}  // namespace nnoops
 
 #endif
---
+++ test/containers/map_test.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+#include "nnoops/binary_trees/binary_search_tree.hpp"
+#include "nnoops/containers/map.hpp"
+
+using namespace nnoops;
+
+TEST(Map, base_test) {}
